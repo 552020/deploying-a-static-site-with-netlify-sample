dbd19e5ccfc2125e0d1cb0c5b1c9f880d3131d7b
552020 <stefanolombardo@posteo.de>
2021-04-26 23:40:12 +0200

    elaborate css and js

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+### Content
+
+Forking-variation on an unity of Codecademy: [Deploying a Static Site with Netlify](https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-react-part-ii/modules/fscp-deploy-a-react-application-with-netlify/articles/deploying-a-static-site-with-netlify)
+
+### Languages
+
+HTML, CSS, JS
+
 ## Deploying a Static Site with Netlify
+
 Hello! This GitHub repo is intended to be used with the article [Deploying a Static Site with Netlify](https://www.codecademy.com/articles/deploying-a-static-site-with-netlify).
 
 Make sure to follow the steps as outlined in the article to see how to use Heroku for your deployment needs!
--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const clapButton = document.getElementById('clap-button');
+const clapButton = document.getElementById("clap-button");
 
-clapButton.addEventListener('click', switchBackground);
+clapButton.addEventListener("click", switchBackground);
+clapButton.addEventListener("click", switchBackgroundButton);
+clapButton.addEventListener("click", switchBackgroundBetween);
+clapButton.addEventListener("click", switchFontColor);
 
 function randomColor() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
 
-  const color = `rgb(${red}, ${green}, ${blue})`
+  const color = `rgb(${red}, ${green}, ${blue})`;
   return color;
 }
 
 function switchBackground() {
   const backgroundColor = randomColor();
   document.body.style.backgroundColor = backgroundColor;
+}
+
+function switchBackgroundBetween() {
+  const backgroundColor = randomColor();
+  document.querySelector(".container").style.backgroundColor = backgroundColor;
+}
+
+function switchBackgroundButton() {
+  const backgroundColor = randomColor();
+  document.querySelector(".button").style.backgroundColor = backgroundColor;
+}
+
+function switchFontColor() {
+  const fontColor = randomColor();
+  document.body.style.color = fontColor;
 }
